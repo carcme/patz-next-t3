d6f429e1cdcfc8d71f1c14d2b3a70efc14dc5bd2
carc <carcmedev@gmail.com>
2023-03-30 17:38:27 +0200

    added next image, some styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
-import "~/styles/globals.css";
+import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
